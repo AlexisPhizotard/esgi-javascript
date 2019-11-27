e4f67f308f4359cfa959510a640d1e14fd0753a2
Alexis Phizotard <aphizotard@aol.com>
2019-11-27 14:18:42 +0100

    test

--- exercice-1/string.js
+++ exercice-1/string.js
@@ -31,13 +31,23 @@ function snake_case(str) {
 
 //leet
 // function leet(str) {
+//     if(typeof str !== "string" || !str ) return '';
+//     // let chrs = {'a':'4','e':'3','i':'1','y':'7','u':'(_)','o':'0'};
+//     // return str.replace(/[aeiouy]/g);
+//     }
 
-// }
 
-console.log(snake_case(""));
-console.log(snake_case("Test"));
-console.log(snake_case("  test"));
-console.log(snake_case("test Test tst"));
-console.log(snake_case(""));
-console.log(snake_case(null));
-console.log(snake_case({}));
+// A => 4
+// E => 3
+// I => 1 
+// O => 0
+// U => (_)
+// Y => 7
+
+console.log(leet("a"));
+console.log(leet("Test"));
+console.log(leet("  test"));
+console.log(leet("test Test tst"));
+console.log(leet(""));
+console.log(leet(null));
+console.log(leet({}));
