6c0f29f81cc6cde60b41787f2f14ec41757ca664
Alexis Phizotard <aphizotard@aol.com>
2019-11-27 16:52:24 +0100

    verlan et yoda

--- exercice-1/string.js
+++ exercice-1/string.js
@@ -26,16 +26,46 @@ function camelCase(str) {
 //snake_case
 function snake_case(str) {
     if(typeof str !== "string" || !str ) return '';
-    return str.replace(/[^a-zA-Z0-9]/g, '_').toLowerCase();
-    //return str.split(" ").join("_").toLowerCase();
+    return strtoLowerCase().replace(/[^a-zA-Z0-9]/g, '_');
 }
 
 //leet
 function leet(str) {
     if(typeof str !== "string" || !str ) return '';
-    return str.replace(/[aA]/g,"4").replace(/[eE]/g,"3").replace(/[iI]/g,"1").replace(/[oO]/g,"0").replace(/[uU]/g,"(_)").replace(/[yY]/g,"7");
+    return str.replace(/[aeiouy]/gi, function(item){
+        switch(item) {
+            case 'a':
+            case 'A':
+                return 4;
+            case 'e':
+            case 'E':
+                return 3;
+            case 'i':
+            case 'I':
+                return 1;
+            case 'o':
+            case 'O':
+                return 0;
+            case 'u':
+            case 'U':
+                return "(_)";
+            case 'y':
+            case 'Y':
+                return 7;
+        }
+    })
 }
 
+function verlan(str) {
+    if(typeof str !== "string" || !str ) return '';
+    return str.split(" ").map(function(word){
+        return word.split("").reverse().join('');
+    }).join(" ");
+}
 
+function yoda(str) {
+    if(typeof str !== "string" || !str ) return '';
+    return str.split(" ").reverse().join(" ");
+}
 
-console.log(leet("anaconda"));
+console.log(yoda("hello world"));
